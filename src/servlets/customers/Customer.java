17ceb5593934a6c49265814e2768b2604ad72ba5
Remi Rudi <Remir18@uia.no>
2019-10-17 15:18:05 +0200

    Auto stash before revert of "Merge remote-tracking branch 'origin/master'"

--- src/servlets/customers/Customer.java
+++ src/servlets/customers/Customer.java
@@ -17,47 +17,82 @@ public class Customer extends HttpServlet {
 
     }
 
-        private int customerID;
-        private String name;
-        private String gender;
-        private String email;
-
-        public Customer(int customerID, String name, String gender, String email) {
-            super();
-            this.name = name;
-            this.gender = gender;
-            this.customerID = customerID;
-            this.email = email;
-        }
-
-        public String getName() {
-            return name;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-        public int getCustomerID() {
-            return customerID;
-        }
-        public void setCustomerID(int customerID) {
-            this.customerID = customerID;
-        }
-        public String getEmail() {
-            return email;
-        }
-        public void setEmail(String email) {
-            this.email = email;
-        }
+    private int customerID;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String customerAddress;
+    private int disabilities;
+    private String email;
 
 
+    public Customer() {
+        super();
+        this.email = email;
+        this.customerID = customerID;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.customerAddress = customerAddress;
+        this.disabilities = disabilities;
 
-        public void setMail(String email) {
 
-            if (email.contains("@")) {
-                this.email = email;
-            } else {
-                System.out.println("You must provide a valid email address");
-            }
-
-        }
     }
+
+
+    public String fullName(String fullName) {
+        fullName = (firstName+lastName);
+        return fullName;
+    }
+
+    public String setFirstName(String firstName) {
+        return firstName;
+    }
+
+    public String setLastName(String lastName) {
+        return lastName;
+    }
+
+    public String setMiddleName (String middleName) {
+        return middleName;
+    }
+
+    public String setCustomerAddress (String customerAddress) {
+        return customerAddress;
+    }
+
+    public int setDisabilities (int disabilities) {
+        return disabilities;
+    }
+
+
+
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
+    public void setMail(String email) {
+
+        if (email.contains("@")) {
+            this.email = email;
+        } else {
+            System.out.println("You must provide a valid email address");
+        }
+
+    }
+}
