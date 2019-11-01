553f2abf7692ce11fa25c8bc095e09ffb27ed6ab
Remi Rudi <Remir18@uia.no>
2019-11-01 10:28:09 +0100

    Employee HTML og print på employeeServlet

--- src/servlets/Employee/EmployeeServlet.java
+++ src/servlets/Employee/EmployeeServlet.java
@@ -1,6 +1,21 @@
 package servlets.Employee;
 
+import servlets.Employee.EmployeeLogic;
+import servlets.Employee.Employee;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "employeeServlet", urlPatterns = {"/EmployeeServlet"})
 
 public class EmployeeServlet extends HttpServlet {
+
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException{
+        response.setContentType("text/html");
+        System.out.println("Hei");
+    }
 }
