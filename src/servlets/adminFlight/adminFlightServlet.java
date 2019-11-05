3dbb32d243b0f77998d89c74329c740baa146c1b
Ole Kristoffer Foss <olekf17@uia.no>
2019-11-05 16:48:39 +0100

    Nye adminfunksjoner, ikke fungerende enda, pushes for å lagres

--- src/servlets/adminFlight/adminFlightServlet.java
+++ src/servlets/adminFlight/adminFlightServlet.java
@@ -1,7 +1,5 @@
 package servlets.adminFlight;
 
-import servlets.flights.Flight;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
