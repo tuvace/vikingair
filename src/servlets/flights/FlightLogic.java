e3cab279fd2cf6511f9342f42de9a9a58f45cd9e
Tuva Cecilie <tce@live.no>
2019-11-21 17:30:41 +0100

    Merg conflict fix

--- src/servlets/flights/FlightLogic.java
+++ src/servlets/flights/FlightLogic.java
@@ -7,10 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import dbcode.dbUtilities;
-<<<<<<< HEAD
-=======
 
->>>>>>> master
 import servlets.customers.Customer;
 import servlets.flights.Flight;
 import javax.sql.DataSource;
